c44d2786b73e34cb85c5d394b88f9a96d521bd0d
sirrotich <rotichtitus12@gmail.com>
2019-05-30 12:40:26 +0300

    view function for edit and upload magge

--- neighbours/views.py
+++ neighbours/views.py
@@ -20,3 +20,33 @@ def profile(request):
     title = f'@{profile.username} Hood Updates'
 
     return render(request, 'profile/profile.html',{'title':title, 'profile':profile,'profile_details':profile_details,'images':images})
+
+
+@login_required(login_url='/accounts/login')
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.profile = request.user
+            # print(f'image is {upload.image}')
+            upload.save()
+            return redirect('profile', username=request.user)
+    else:
+        form = ImageForm()
+    
+    return render(request, 'profile/upload_image.html', {'form':form})
+
+@login_required(login_url='/accounts/login')
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            edit = form.save(commit=False)
+            edit.user = request.user
+            edit.save()
+            return redirect('profile.html')
+    else:
+        form = ProfileForm()
+
+    return render(request, 'profile/edit_profile.html', {'form':form})
