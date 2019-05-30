989df63ee0fd0a29eb173511bd096661f1d5e62c
sirrotich <rotichtitus12@gmail.com>
2019-05-30 08:54:30 +0300

    create templates

--- neighbours/urls.py
+++ neighbours/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', views.home,name ='home')
+    url(r'^$', views.home,name ='home'),
+    url(r'^index', views.index, name = 'index')
 ]
