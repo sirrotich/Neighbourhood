39ae5e9a3718e7b86d322870ba17448c5d63884d
sirrotich <rotichtitus12@gmail.com>
2019-05-30 12:39:11 +0300

    generate a review class

--- neighbours/models.py
+++ neighbours/models.py
@@ -62,3 +62,17 @@ class Image(models.Model):
     def get_all_images(cls):
         images = Image.objects.all()
         return images
+
+class Comments(models.Model):
+    comment = HTMLField()
+    posted_on = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_comment(self):
+        self.save()
+    
+    @classmethod
+    def get_comments_by_images(cls, id):
+        comments = Comments.objects.filter(image__pk = id)
+        return comments
