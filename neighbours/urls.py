009c36ed1e314469640c7938cb3f87f1531c2cfa
sirrotich <rotichtitus12@gmail.com>
2019-05-30 12:49:00 +0300

    edit urls to views

--- neighbours/urls.py
+++ neighbours/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     url(r'^$', views.home,name ='home'),
     url(r'^index', views.index, name = 'index'),
     url(r'user/(?P<username>\w+)', views.profile,name='profile'),
+    url(r'^upload/$', views.upload_image, name='upload_image'),
+    url(r'^accounts/edit/',views.edit_profile, name='edit_profile'),
+    url(r'^image/(?P<image_id>\d+)', views.single_image, name='single_image'),
+    url(r'^search/', views.search, name='search')
 ]
