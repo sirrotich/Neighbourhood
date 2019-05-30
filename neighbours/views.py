08db90fdc06fe9466dc66895448e7d97d4c46a92
sirrotich <rotichtitus12@gmail.com>
2019-05-30 12:47:54 +0300

    import django requirements to view

--- neighbours/views.py
+++ neighbours/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
-
-from .models import  Profile
+from django.http  import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm, ImageForm, ProfileForm, CommentForm
+# from .emails import send_activation_email
+# from .tokens import account_activation_token
+from .models import Image, Profile, Comments
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
