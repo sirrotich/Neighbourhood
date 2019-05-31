afaf5c9c1a347abc68da4c1a4e6e93b405520875
sirrotich <rotichtitus12@gmail.com>
2019-05-31 13:58:15 +0300

    push to heroku

--- community/settings.py
+++ community/settings.py
@@ -14,6 +14,7 @@ import os
 import django_heroku
 import dj_database_url
 from decouple import config,Csv
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -113,7 +114,7 @@ WSGI_APPLICATION = 'community.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tribune',
+        'NAME': 'neighbourhood',
         'USER': 'kipngeno',
         'PASSWORD':'1234',
     }
