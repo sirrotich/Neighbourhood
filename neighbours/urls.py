c2e03b40d9f780b0df409e67b657ba8cb90f087f
sirrotich <rotichtitus12@gmail.com>
2019-05-31 08:17:57 +0300

    connect urls with static

--- neighbours/urls.py
+++ neighbours/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.home,name ='home'),
@@ -12,3 +13,5 @@ urlpatterns = [
     url(r'^image/(?P<image_id>\d+)', views.single_image, name='single_image'),
     url(r'^search/', views.search, name='search')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- neighbours/views.py
+++ neighbours/views.py
@@ -12,15 +12,17 @@ from .forms import SignupForm, ImageForm, ProfileForm, CommentForm
 # from .tokens import account_activation_token
 from .models import Image, Profile, Comments
 # Create your views here.
+
 def home(request):
     return render(request, 'home.html')
 
+@login_required(login_url='/accounts/login')
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
     if request.user.is_authenticated():
-        return redirect('home')
+        return redirect('index')
     else:
         if request.method == 'POST':
             form = SignupForm(request.POST)
@@ -34,16 +36,16 @@ def signup(request):
                 return HttpResponse('Confirm your email address to complete registration')
         else:
             form = SignupForm()
-            return render(request, 'registration/registration_form.html',{'form':form})
+            return render(request, 'registration/signup.html',{'form':form})
 
-def profile(request):
+def profile(request,username):
     profile = User.objects.get(username=username)
     try:
         profile_details = Profile.get_by_id(profile.id)
     except:
         profile_details = Profile.filter_by_id(profile.id)
     
-    images = Images.get_profile_images(profile.id)
+    images = Image.get_profile_images(profile.id)
     title = f'@{profile.username} Hood Updates'
 
     return render(request, 'profile/profile.html',{'title':title, 'profile':profile,'profile_details':profile_details,'images':images})
