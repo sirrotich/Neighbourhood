3033aeb2b2b9a29d77f2eee845994bb69088b128
sirrotich <rotichtitus12@gmail.com>
2019-06-01 14:28:35 +0300

    deployment trial

--- community/settings.py
+++ community/settings.py
@@ -47,7 +47,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(','))
+ALLOWED_HOSTS = config('ALLOWED_HOSTS'))
 
 # Application definition
 
